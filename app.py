30eb9fce01f05a7f69f0b72dc370f089dfafd016
Esdros <esdros@Esdross-MBP.fios-router.home>
2025-07-08 10:17:18 -0400

    Updated app.py file again

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-vehicles = pd.read_csv('/vehicles_us.csv')
+vehicles = pd.read_csv('./vehicles_us.csv')
 st.header("This is my header")
 
 #Plotly Express Histogram
